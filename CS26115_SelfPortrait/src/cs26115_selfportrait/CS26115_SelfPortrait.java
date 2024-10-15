88902a7ef39274dfdea21e99f35624c25e87f125
Andrea Dayo <72564321+andreadayo@users.noreply.github.com>
2024-10-15 18:08:47 +0800

    init

---
+++ CS26115_SelfPortrait/src/cs26115_selfportrait/CS26115_SelfPortrait.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cs26115_selfportrait;
+
+/**
+ *
+ * @author Dhea
+ */
+public class CS26115_SelfPortrait {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
